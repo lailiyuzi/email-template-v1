7c7d5db4007b4a025755698207bd8a39788f3e33
lailiyuzi <nurlaili.yuzi2021@gmail.com>
2022-12-12 11:48:20 +0800

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+
+import 'primereact/resources/primereact.min.css';
+import "primeicons/primeicons.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Create} from './template';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Create />
     </div>
   );
 }
---
+++ src/template/create_template.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Card } from 'primereact/card';
+import './create.css';
+
+
+const Create = () => {
+  return (
+              <div className="slider-thumb">
+
+                <div className="Container">
+
+                <h5>Create</h5>
+                 <Card title="Simple Card" style={{ width: '25rem', marginBottom: '2em' }}>
+               
+                <p className="m-0" style={{lineHeight: '1.5'}}>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt
+                    quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!</p>
+               
+              </Card>
+
+                </div>
+              
+              </div>
+              
+            
+           
+  );
+}
+
+export default Create;
